89c738ddd5e9752b24de6af3f92201300e21ce45
Abraham Krisnanda S <abraham.krisnanda@gmail.com>
2013-10-07 17:56:59 +0700

    Parking

--- FlyHigh/Models/Schedule.cs
+++ FlyHigh/Models/Schedule.cs
@@ -25,7 +25,33 @@ namespace FlyHigh.Models
         public int FlightId { get; set; }
         public System.DateTime Date { get; set; }
         public int PlaneId { get; set; }
-    
+
+        public System.DateTime DepartureTime
+        {
+            get
+            {
+                return new DateTime(Date.Year, Date.Month, Date.Day, Flight.Departure.Hours, Flight.Departure.Minutes,
+                    Flight.Departure.Seconds);
+            }
+        }
+
+        public System.DateTime ArrivalTime
+        {
+            get
+            {
+                DateTime d = DepartureTime.Add(TimeSpan.FromHours(Flight.Duration));
+                return d;
+            }
+        }
+
+        public TimeSpan ParkingDuration
+        {
+            get
+            {
+                return (ArrivalTime - DepartureTime);
+            }
+        }
+
         public virtual Flight Flight { get; set; }
         public virtual ICollection<PilotSchedule> PilotSchedules { get; set; }
         public virtual Plane Plane { get; set; }
