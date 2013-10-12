47f75686db384e3372fd1d16b55c06772d77a47d
Edward Samuel <edwardsamuel92@gmail.com>
2013-10-12 10:48:20 +0700

    Authentication

--- FlyHigh/Controllers/AccountController.cs
+++ FlyHigh/Controllers/AccountController.cs
@@ -10,11 +10,11 @@ using Microsoft.Web.WebPages.OAuth;
 using WebMatrix.WebData;
 using FlyHigh.Filters;
 using FlyHigh.Models;
+using MySql.Web.Security;
 
 namespace FlyHigh.Controllers
 {
     [Authorize]
-    [InitializeSimpleMembership]
     public class AccountController : Controller
     {
         //
@@ -263,7 +263,7 @@ namespace FlyHigh.Controllers
             if (ModelState.IsValid)
             {
                 // Insert a new user into the database
-                using (UsersContext db = new UsersContext())
+                using (SimpleMembershipTestDbContext db = new SimpleMembershipTestDbContext())
                 {
                     UserProfile user = db.UserProfiles.FirstOrDefault(u => u.UserName.ToLower() == model.UserName.ToLower());
                     // Check if user already exists
--- FlyHigh/Global.asax.cs
+++ FlyHigh/Global.asax.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using FlyHigh.Filters;
+using FlyHigh.Models;
+using MySql.Web.Security;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -23,6 +28,39 @@ namespace FlyHigh
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            Database.SetInitializer<SimpleMembershipTestDbContext>(null);
+            try
+            {
+                using (var context = SimpleMembershipTestDbContext.CreateContext())
+                {
+                    if (context.Database.Exists() == false)
+                    {
+                        // Create the SimpleMembership database without Entity Framework migration schema
+                        ((IObjectContextAdapter) context).ObjectContext.CreateDatabase();
+                    }
+                }
+
+                MySqlWebSecurity.InitializeDatabaseConnection("ErlanggaMySql");
+
+                //const string ADMIN_ROLES = "Administrators";
+                //const string ADMIN_USER = "admin";
+
+                //if (System.Web.Security.Roles.RoleExists(ADMIN_ROLES) == false)
+                //{
+                //    System.Web.Security.Roles.CreateRole(ADMIN_ROLES);
+
+                //    if (WebSecurity.UserExists(ADMIN_USER) == false)
+                //        WebSecurity.CreateUserAndAccount(ADMIN_USER, "password");
+
+                //    if (Array.Exists(System.Web.Security.Roles.GetRolesForUser(ADMIN_USER), f => f == ADMIN_ROLES) == false)
+                //        System.Web.Security.Roles.AddUserToRole(ADMIN_USER, ADMIN_ROLES);
+                //}
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("The ASP.NET Simple Membership database could not be initialized. For more information, please see http://go.microsoft.com/fwlink/?LinkId=256588", ex);
+            }
         }
     }
 }
