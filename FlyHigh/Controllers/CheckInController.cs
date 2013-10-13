c6eb077b8c3b174cafed6d9ef1a8509bad41cd8d
raymond <raymond_pluto@hotmail.com>
2013-10-13 23:17:02 +0700

    ubah max baggage

--- FlyHigh/Controllers/CheckInController.cs
+++ FlyHigh/Controllers/CheckInController.cs
@@ -12,7 +12,6 @@ namespace FlyHigh.Controllers
     public class CheckInController : Controller
     {
         private static double BAGGAGE_CHARGE_MULTIPLIER = 50000;
-        private static double MAX_WEIGHT_PER_PERSON = 20;
 
         private ErlanggaEntities db = new ErlanggaEntities();
 
@@ -58,7 +57,12 @@ namespace FlyHigh.Controllers
                 }
             }
             var tickets = db.Tickets.Include(ps => ps.Booking).Where(ps => ps.BookingId == model.booking.BookingId && ps.CheckIn != null).ToList();
-            //model.booking.Tickets = (ICollection<Ticket>)tickets;
+            var ticketId = tickets.ElementAt(0).TicketId;
+            TicketInstance ti = db.TicketInstances.Where(ps => ps.TicketId == ticketId).FirstOrDefault();
+            PlaneClass pc = db.PlaneClasses.Where(ps => ps.ClassId == ti.ClassId).FirstOrDefault();
+
+
+            ViewBag.maxWeight = pc.FreeBaggage;
             return View("Baggage", tickets);
         }
 
@@ -83,15 +87,24 @@ namespace FlyHigh.Controllers
                 db.SaveChanges();
 
                 double totalWeight = 0;
+
+                long ticketId = baggageToAdd.TicketId;
+            TicketInstance ti = db.TicketInstances.Where(ps => ps.TicketId == ticketId).FirstOrDefault();
+            PlaneClass pc = db.PlaneClasses.Where(ps => ps.ClassId == ti.ClassId).FirstOrDefault();
+
                 var baggages = db.Baggages.Where(ps => ps.TicketId == baggageToAdd.TicketId).ToList();
                 foreach (var baggage in baggages)
                 {
                     totalWeight += baggage.Weight;
                 }
-                if (totalWeight > MAX_WEIGHT_PER_PERSON)
+
+                double maxWeight = (double) pc.FreeBaggage;
+                ViewBag.maxWeight = maxWeight;
+
+                if (totalWeight > maxWeight)
                 {
                     var ticketToUpdate = db.Tickets.Where(ps => ps.TicketId == baggageToAdd.TicketId).Single();
-                    ticketToUpdate.BaggageCharge = (decimal)((totalWeight - MAX_WEIGHT_PER_PERSON) * BAGGAGE_CHARGE_MULTIPLIER);
+                    ticketToUpdate.BaggageCharge = (decimal)((totalWeight - maxWeight) * BAGGAGE_CHARGE_MULTIPLIER);
                     db.Entry(ticketToUpdate).State = EntityState.Modified;
                 }
 
@@ -100,14 +113,25 @@ namespace FlyHigh.Controllers
 
                 var bookingId = db.Tickets.Where(ps => ps.TicketId == baggageToAdd.TicketId).FirstOrDefault().BookingId;
 
-                var booking = db.Bookings.Include(ps => ps.Tickets).Where(ps => ps.BookingId == bookingId).FirstOrDefault();
+                var tickets = db.Tickets.Include(ps => ps.Booking).Where(ps => ps.BookingId == bookingId && ps.CheckIn != null).ToList();
 
+                //var booking = db.Bookings.Include(ps => ps.Tickets).Where(ps => ps.BookingId == bookingId).FirstOrDefault();
+
+                //List<Ticket> tickets = new List<Ticket>();
+
+                //foreach (var ticket in booking.Tickets)
+                //{
+                //    if (ticket.CheckIn != null)
+                //    {
+                //        tickets.Add(ticket);
+                //    }
+                //}
                 //var tickets = db.Tickets.Include(ps => ps.Booking).Where(ps => ps.BookingId == baggageToAdd.Ticket.BookingId && ps.CheckIn != null).ToList()
                 //var tickets2 = db.Tickets.Include(ps => ps.Booking);
                 //var tickets3 = tickets2.Where(ps => ps.BookingId == baggageToAdd.Ticket.BookingId && ps.CheckIn != null).ToList();
                 //var tickets = tickets3.ToList();
 
-                return View("Baggage", booking.Tickets.ToList());
+                return View("Baggage", tickets);
             }
             //ViewBag.FromAirportId = new SelectList(db.Airports, "AirportId", "AirportCode", flight.FromAirportId);
             //ViewBag.ToAirportId = new SelectList(db.Airports, "AirportId", "AirportCode", flight.ToAirportId);
